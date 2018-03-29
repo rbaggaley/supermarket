5e18789a2f4b058327bcb2d504fcb94a09861911
student <b6042302@my.shu.ac.uk>
2018-03-29 09:24:06 +0100

    Created databases and configured

--- hrSystem/grails-app/init/hrsystem/BootStrap.groovy
+++ hrSystem/grails-app/init/hrsystem/BootStrap.groovy
@@ -4,7 +4,7 @@ class BootStrap {
 
     def init = { servletContext ->
 	
-	def Andy = new Manager(
+	/*def Andy = new Manager(
 
 		fullName:'Andy Williams',
 		userName:'andy123',
@@ -28,8 +28,14 @@ class BootStrap {
 		numberOfEmployees:5,
 		sectionName:'bakery',
 		description:'a description').save()
-	
 
+	def TeamB = new Team(
+		
+		teamName:'teamB',
+		numberOfEmployees:8,
+		sectionName: 'fresh produce',
+		description:'a description').save()
+	
 	def Barry = new TeamLeader(
 
 		fullName:'Barry Nolan',
@@ -42,12 +48,31 @@ class BootStrap {
 		manager: Davy,
 		team: TeamA).save()
 
+	def Darren = new TeamLeader(
+
+		fullName:'Darren White',
+		department:'fresh produce',
+		employeeID:'DW123',
+		sectionName:'bakery',
+		officePhone:'01123 234356',
+		leaderEmail:'darren@aol.com',
+		password:'darren123',
+		manager: Andy,
+		team: TeamB).save()
+
 	def shiftMorning = new Shift(
 		
 		timeOfDay:'morning',
 		dayOfWeek:'monday',
 		numberOfHours:2,
 		startingTime:'9:00am').save()
+
+	def shiftAfternoon = new Shift(
+		
+		timeOfDay:'afternoon',
+		dayOfWeek:'Wednesday',
+		numberOfHours:2,
+		startingTime:'12:00pm').save()
 
 	def Craig = new Employee(
 
@@ -63,13 +88,35 @@ class BootStrap {
 		manager: Andy,
 		teamLeader: Barry).save()
 
-	
+	def Eric = new Employee(
+
+		fullName:'Eric Cartman',
+		dateOfBirth:new Date ('09/09/2018'),
+		residence:'sheffield',
+		hourlyRate:6,
+		employeeID:'EC123',
+		dateEmployed:new Date('09/09/2017'),
+		taxCode:'TX1',
+		contract:'full-time',
+		shift: shiftAfternoon,
+		manager: Andy,
+		teamLeader: Barry).save()	
 
 	def Task1 = new Task(
 
 		taskName:'task1',
 		numberOfPeople:4,
 		sectionName:'bakery',
+		department:'groceries',
+		timeRequired:'1 hour',
+		description:'cleaning',
+		taskCompleted:false).save()
+
+	def Task2 = new Task(
+
+		taskName:'task2',
+		numberOfPeople:4,
+		sectionName:'fruit',
 		department:'groceries',
 		timeRequired:'1 hour',
 		description:'cleaning',
@@ -83,10 +130,10 @@ class BootStrap {
 	Task1.addToShifts(shiftMorning)
 	TeamA.addToEmployees(Craig)
 	TeamA.addToTasks(Task1)
-	TeamA.addToShifts(shiftMorning)
-			
-		
-		
+	TeamA.addToShifts(shiftMorning)*/
+
+
+					
 	
    } 
     def destroy = {
